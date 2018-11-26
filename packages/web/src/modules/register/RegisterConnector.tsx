6d33bfc2e7600e0b7a1b410dadab92068122cb50
li liu <liliu@lideMacBook-Pro.local>
2018-11-26 15:50:47 +0800

    .

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
+import { RegisterController} from '@abb/controller'
 import { RegisterView } from '../view/RegisterView';
 
 export class RegisterConnector extends React.PureComponent {
-    dummySubmit = async (values: any)=> {
-        console.log(values)
-        return null
-    }
+
     render() {
-        return <RegisterView submit={this.dummySubmit} />
+        return (
+            <RegisterController>
+                {({ submit }) => <RegisterView submit={submit} />} 
+            </RegisterController>
+        )
     }
 }
